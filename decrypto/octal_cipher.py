b96514cee70f8e4bd8e825dc1580bb5f2fe30025
pyguru123 <pyguru123@gmail.com>
2021-10-04 00:15:02 +0530

    added octal & hexadecimal cipher

--- decrypto/octal_cipher.py
+++ decrypto/octal_cipher.py
@@ -1,37 +1,35 @@
-# Binary Cipher implimentation
-
-class BinaryCipher:
+class OctalCipher:
 	def __init__(self):
 		''' This is a python implementation of Binary Cipher. More about it can
-		be read here : https://en.wikipedia.org/wiki/Binary-to-text_encoding'''
+		be read here : https://en.wikipedia.org/wiki/Octal'''
 
-	def encrypt(self, msg: [int, str]) -> str:
+	def encrypt(self, msg: [int, str] ) -> str:
 		result = None
 
 		if isinstance(msg, int):
-			binary = bin(msg)
-			result = int(str(binary)[2:])
+			octal = oct(msg)
+			result = int(str(octal)[2:])
 
 		elif isinstance(msg, str):
 			result = ''
 			for ele in msg:
 				value = ord(ele)
-				binary_value = bin(value) + ' '
-				result += binary_value[2:]
+				octal_value = oct(value) + ' '
+				result += octal_value[2:]
 
 		return result
 
-	def decrypt(self, msg: [int, str]) -> str:
+	def decrypt(self, msg: [int, str] ) -> str:
 		result = None
 
 		if isinstance(msg, int):
-			result = int(str(msg), 2)
+			result = int(str(msg), 8)
 
 		elif isinstance(msg, str):
 			result = ''
 			splitted_list = msg.split()
 			for ele in splitted_list:
-				value = int(ele, 2)
+				value = int(ele, 8)
 				result += chr(value)
 
 		return result
