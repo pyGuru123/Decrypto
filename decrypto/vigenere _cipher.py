31d198259330af87646d4255adac52519bc2f763
Murugan K <murugank0204@gmail.com>
2021-10-12 17:39:17 +0530

    refactored to provide inbetween spaces and converted the texts to upper

--- decrypto/vigenere _cipher.py
+++ decrypto/vigenere _cipher.py
@@ -4,26 +4,34 @@ class VigenereCipher:
 
     @staticmethod
     def encrypt(msg: str, key: str) -> str:
+        msg, key  = msg.upper(), key.upper() 
         if len(msg) > len(key):
-            # Generates the key in a cyclic manner until it's length equal to the length of msg
+            # Generates the key in a cyclic manner until it's length equal to the length of text
             for i in range(len(msg) - len(key)):
                 key += key[i % len(key)]
-        result = ''
-        for i in range(len(msg)):
-            x = (ord(msg[i]) + ord(key[i])) % 26
-            x += ord('A')
-            result+= chr(x)
-        return result
+        encrypted_words = []
+        for text in msg.split():
+            result = ''
+            for i in range(len(text)):
+                x = (ord(text[i]) + ord(key[i])) % 26
+                x += ord('A')
+                result+= chr(x)
+            encrypted_words.append(result)
+        return ' '.join(encrypted_words)
 
     @staticmethod
     def decrypt(msg: str,key: str) -> str:
+        msg, key  = msg.upper(), key.upper() 
         if len(msg) > len(key):
             # Generates the key in a cyclic manner until it's length equal to the length of msg
             for i in range(len(msg) - len(key)):
                 key += key[i % len(key)]
-        result = ''
-        for i in range(len(msg)):
-            x = (ord(msg[i]) - ord(key[i]) + 26) % 26
-            x += ord('A')
-            result += chr(x)
-        return result
+        decrypted_words = []
+        for word in msg.split():
+            result = ''     
+            for i in range(len(word)):
+                x = (ord(word[i]) - ord(key[i]) + 26) % 26
+                x += ord('A')
+                result += chr(x)
+            decrypted_words.append(result)
+        return ' '.join(decrypted_words)
