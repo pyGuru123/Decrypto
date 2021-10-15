90c0e39f1ae8e95aa3cd21620f7ecf9ce294e8b1
CHANDANA SAMINENI <chandanasamineni23@gmail.com>
2021-10-15 22:34:49 +0530

    RSA Algorithm Python Implementation

---
+++ decrypto/src/rsa.py
@@ -0,0 +1,20 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import binascii
+ 
+keyPair = RSA.generate(3072)
+ 
+pubKey = keyPair.publickey()
+print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+pubKeyPEM = pubKey.exportKey()
+print(pubKeyPEM.decode('ascii'))
+ 
+print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+privKeyPEM = keyPair.exportKey()
+print(privKeyPEM.decode('ascii'))
+ 
+#encryption
+msg = 'A message for encryption'
+encryptor = PKCS1_OAEP.new(pubKey)
+encrypted = encryptor.encrypt(msg)
+print("Encrypted:", binascii.hexlify(encrypted))
