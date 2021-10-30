a3e5b10582d18223d509ce5cd98174dd002336b7
Ayush Gupta <72538480+GuptaAyush19@users.noreply.github.com>
2021-10-30 15:10:14 +0530

    Resolve imports and documentation

--- decrypto/__init__.py
+++ decrypto/__init__.py
@@ -1,14 +1,34 @@
-from decrypto.src.affine_cipher import AffineCipher
-from decrypto.src.ascii_cipher import AsciiCipher
-from decrypto.src.atbash_cipher import AtbashCipher
-from decrypto.src.bacon_cipher import BaconCipher
-from decrypto.src.base64_cipher import Base64Cipher
-from decrypto.src.binary_cipher import BinaryCipher
-from decrypto.src.caesar_cipher import CaesarCipher
-from decrypto.src.hexadecimal_cipher import HexadecimalCipher
-from decrypto.src.morse_code_cipher import MorseCodeCipher
-from decrypto.src.octal_cipher import OctalCipher
-from decrypto.src.reverse_cipher import ReverseCipher
-from decrypto.src.roman_numeral_cipher import RomanNumeralCipher
-from decrypto.src.rot13_cipher import ROT13Cipher
-from decrypto.src.vigenere_cipher import VigenereCipher
+from .src.affine_cipher import AffineCipher
+from .src.ascii_cipher import AsciiCipher
+from .src.atbash_cipher import AtbashCipher
+from .src.bacon_cipher import BaconCipher
+from .src.base64_cipher import Base64Cipher
+from .src.binary_cipher import BinaryCipher
+from .src.caesar_cipher import CaesarCipher
+from .src.hexadecimal_cipher import HexadecimalCipher
+from .src.morse_code_cipher import MorseCodeCipher
+from .src.octal_cipher import OctalCipher
+from .src.reverse_cipher import ReverseCipher
+from .src.roman_numeral_cipher import RomanNumeralCipher
+from .src.rot13_cipher import ROT13Cipher
+from .src.vigenere_cipher import VigenereCipher
+
+__all__ = [
+    "AffineCipher",
+    "AsciiCipher",
+    "AtbashCipher",
+    "BaconCipher",
+    "Base64Cipher",
+    "BinaryCipher",
+    "CaesarCipher",
+    "HexadecimalCipher",
+    "MorseCodeCipher",
+    "OctalCipher",
+    "ReverseCipher",
+    "RomanNumeralCipher",
+    "ROT13Cipher",
+    "VigenereCipher"
+]
+
+__author__ = "Prajjwal Pathak; https://github.com/pyGuru123/Decrypto"
+__license__ = "MIT License"
