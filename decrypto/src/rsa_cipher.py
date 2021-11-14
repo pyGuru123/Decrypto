92f268d299d1e07cc3179ceb785b2fd62335e856
pyguru123 <pyguru123@gmail.com>
2021-11-15 00:57:37 +0530

    completed rsa

--- decrypto/src/rsa_cipher.py
+++ decrypto/src/rsa_cipher.py
@@ -1,45 +1,54 @@
 import random
 
 class RSACipher():
-    def __init__(self, p, q):
-        self.p = p
-        self.q = q
-        self.n = self.p * self.q
-        self.totient = self.get_totient()
-        self.e = self.get_e()
-        self.d = self.modular_inverse(self.e, self.totient)
+    def __init__(self):
+        """This is a python implementation of RSA Cipher"""
 
-    def get_totient(self):
-        return (self.p - 1) * (self.q - 1)
+    @staticmethod
+    def get_totient(p, q):
+        return (p - 1) * (q - 1)
 
-    def gcd(self, a, b):
+    @staticmethod
+    def gcd(a, b):
         if b == 0:
             return a
         else:
-            return self.gcd(b, a % b)
+            return RSACipher.gcd(b, a % b)
 
-    def get_e(self):
-        e = random.randint(2, self.totient - 1)
-        while self.gcd(e, self.totient) != 1:
-            e = random.randint(2, self.totient - 1)
+    @staticmethod
+    def get_e(totient):
+        e = random.randint(2, totient - 1)
+        while RSACipher.gcd(e, totient) != 1:
+            e = random.randint(2, totient - 1)
 
         return e
 
-    def modular_inverse(self, a, m):
+    @staticmethod
+    def modular_inverse(a, m):
         for x in range(1, m):
             if (((a%m) * (x%m)) % m == 1):
                 return x
         return -1
 
-    def generate_keys(self):
+    @staticmethod
+    def generate_keys(p, q):
+        p = p
+        q = q
+        n = p * q
+        totient = RSACipher.get_totient(p, q)
+        # e = RSACipher.get_e(totient)
+        e = 563
+        d = RSACipher.modular_inverse(e, totient)
+
         keys = {
-            'public_key' : (self.n, self.e),
-            'private_key' : (self.p, self.q, self.d)
+            'public_key' : (n, e),
+            'private_key' : (p, q, d)
         }
 
         return keys
 
-    def encrypt(self, msg, public_key):
+    @staticmethod
+    def encrypt(msg, public_key):
         n, e = public_key
         if isinstance(msg, int):
             result = (msg ** e) % n
@@ -50,7 +59,8 @@ class RSACipher():
 
         return result
 
-    def decrypt(self, msg, private_key):
+    @staticmethod
+    def decrypt(msg, private_key):
         p, q, d = private_key
         n = p * q
         if isinstance(msg, int):
@@ -61,16 +71,3 @@ class RSACipher():
                 result += chr((ord(ch) ** d) % n)
 
         return result
-
-cipher = RSACipher(53, 59)
-message = 'hello world'
-keys = cipher.generate_keys()
-encrypted = cipher.encrypt(message, keys['public_key'])
-decrypted = cipher.decrypt(message, keys['private_key'])
-print(keys)
-print(encrypted)
-print(decrypted)
-
-
-
-# 
