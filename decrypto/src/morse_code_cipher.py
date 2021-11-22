8121c6e13ccf6bf1301838ea8445e0291b9177ae
pyguru123 <pyguru123@gmail.com>
2021-11-22 23:11:08 +0530

    optimised morse code cipher

--- decrypto/src/morse_code_cipher.py
+++ decrypto/src/morse_code_cipher.py
@@ -2,60 +2,46 @@ class MorseCodeCipher:
     def __init__(self):
         """ This is a python implementation of MorseCode Cipher"""
 
-        self.morse_dict = {'A': '.-', 'B': '-...',
-                           'C': '-.-.', 'D': '-..', 'E': '.',
-                           'F': '..-.', 'G': '--.', 'H': '....',
-                           'I': '..', 'J': '.---', 'K': '-.-',
-                           'L': '.-..', 'M': '--', 'N': '-.',
-                           'O': '---', 'P': '.--.', 'Q': '--.-',
-                           'R': '.-.', 'S': '...', 'T': '-',
-                           'U': '..-', 'V': '...-', 'W': '.--',
-                           'X': '-..-', 'Y': '-.--', 'Z': '--..',
-                           '1': '.----', '2': '..---', '3': '...--',
-                           '4': '....-', '5': '.....', '6': '-....',
-                           '7': '--...', '8': '---..', '9': '----.',
-                           '0': '-----', ',': '--..--', '.': '.-.-.-',
-                           '?': '..--..', '/': '-..-.', '-': '-....-',
-                           '(': '-.--.', ')': '-.--.-'
-                           }
+        self.morse_dict = {
+            'A': '.-', 'B': '-...',
+            'C': '-.-.', 'D': '-..', 'E': '.',
+            'F': '..-.', 'G': '--.', 'H': '....',
+            'I': '..', 'J': '.---', 'K': '-.-',
+            'L': '.-..', 'M': '--', 'N': '-.',
+            'O': '---', 'P': '.--.', 'Q': '--.-',
+            'R': '.-.', 'S': '...', 'T': '-',
+            'U': '..-', 'V': '...-', 'W': '.--',
+            'X': '-..-', 'Y': '-.--', 'Z': '--..',
+            '1': '.----', '2': '..---', '3': '...--',
+            '4': '....-', '5': '.....', '6': '-....',
+            '7': '--...', '8': '---..', '9': '----.',
+            '0': '-----', ',': '--..--', '.': '.-.-.-',
+            '?': '..--..', ' ': '/', '-': '-....-',
+            '(': '-.--.', ')': '-.--.-'
+        }
 
         self.reverse_morse = {
-            self.morse_dict[key]: key for key in self.morse_dict}
+            self.morse_dict[key]: key for key in self.morse_dict
+        }
 
     def encrypt(self, text: str) -> str:
         result = ''
-
         for ch in text.upper():
-            if ch != ' ':
-                val = self.morse_dict.get(ch, ch)
-                result += (val + ' ')
-            else:
-                result += ' '
+            result += self.morse_dict.get(ch, ch)
+            result += ' '
 
-        return result
+        return result.strip()
 
     def decrypt(self, text: str) -> str:
-        text = text.strip(' ') + ' '
         result = ''
-
-        i = 0
-        char = ''
-        space_ct = 0
-
-        while i < len(text):
-            if text[i] == ' ':
-                if space_ct == 0:
-                    result += self.reverse_morse.get(char, char)
-                    space_ct += 1
-                    char = ''
-                else:
-                    space_ct += 1
-            else:
-                if space_ct > 0:
-                    result += ' ' * (space_ct - 1)
-                    space_ct = 0
-                char += text[i]
-
-            i += 1
+        for ch in text.split():
+            result += self.reverse_morse.get(ch, ch)
 
         return result
+
+data = 'hello world 123 !'
+cipher = MorseCodeCipher()
+encrypted = cipher.encrypt(data)
+decrypted = cipher.decrypt(encrypted)
+print(encrypted)
+print(decrypted)
